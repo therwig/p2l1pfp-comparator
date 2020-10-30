e161513c591f605571b99149f16d307a93f957bd
Christian Herwig <therwig@users.noreply.github.com>
2020-10-29 21:57:50 -0500

    new emulator

--- example_data/emulator/rename.sh
+++ example_data/emulator/rename.sh
@@ -1,4 +1,4 @@
-prefix="oct29"
+prefix="oct29v2"
 cp /home/therwig/data/sandbox/GlobalCorrelator_HLS/tagged_inputs/${prefix}* .
 mv ${prefix}_inputs_converted.txt inputs_converted.txt 
 mv ${prefix}_inputs.txt           inputs.txt           
